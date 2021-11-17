39adc641304fce00681d2f98f241240cb02893de
Wataru Onishi <62089701+Wataru-Onishi@users.noreply.github.com>
2021-11-17 13:16:25 +0900

    Update 2AxisArm.py

--- 2AxisArm.py
+++ 2AxisArm.py
@@ -106,8 +106,8 @@ def main():
 
             kalman(0,roll)
 
-            # X_servo_angle(theta[0])
-            X_servo_angle(roll)
+            X_servo_angle(theta[0])
+            # X_servo_angle(roll)
 
             # print('{:4.3f}, {:4.3f},' .format(theta[0], theta[1]))
             print('{:4.3f}, {:4.3f},' .format(roll, theta[0]))
